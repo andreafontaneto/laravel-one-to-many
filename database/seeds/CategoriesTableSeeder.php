393112e741f8338918ca2f96f476e90d7b585841
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-06 22:22:57 +0100

    make CategoriesTableSeeder + data

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = ['HTML', 'PHP', 'CSS', 'JavaScript'];
+
+        foreach($data as $cat){
+
+            $new_cat = new Category();
+            $new_cat->name = $cat;
+            $new_cat->slug = Str::slug($cat, '-');
+            $new_cat->save();
+
+        };
+    }
+}
